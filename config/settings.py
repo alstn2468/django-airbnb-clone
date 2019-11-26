0c78e4b591f19d050ac1efad51e649ed54cf0674
alstn2468 <alstn2468_@naver.com>
2019-11-26 10:29:44 +0900

    #11 Set LANGUAGE_CODE ko-kr in settings.py

--- config/settings.py
+++ config/settings.py
@@ -111,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "ko-kr"
 
 TIME_ZONE = "Asia/Seoul"
 
