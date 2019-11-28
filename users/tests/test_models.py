414bd6ebae097126dd13a3d52947fafb58a8a88e
alstn2468 <alstn2468_@naver.com>
2019-11-28 22:31:47 +0900

    #47 Fix user model test username

--- users/tests/test_models.py
+++ users/tests/test_models.py
@@ -6,14 +6,14 @@ from users.models import User
 class UserModelTest(TestCase):
     @classmethod
     def setUpTestData(cls):
-        User.objects.create_user("test")
+        User.objects.create_user("test_user_1")
 
     def test_user_create_success(self):
-        user = User.objects.create_user("test1")
-        self.assertEqual("test1", user.username)
+        user = User.objects.create_user("test_user_2")
+        self.assertEqual("test_user_2", user.username)
         self.assertEqual("User", user.__class__.__name__)
 
     def test_user_create_fail(self):
         with self.assertRaises(IntegrityError):
-            User.objects.create_user("test")
+            User.objects.create_user("test_user_1")
 
