1c6ad33c5e00d7d17c0a34d8c668598a59ad46ab
alstn2468 <alstn2468_@naver.com>
2019-11-28 21:19:11 +0900

    #44 Delete assign operator in test_models.py in Users app

--- users/tests/test_models.py
+++ users/tests/test_models.py
@@ -5,7 +5,7 @@ from users.models import User
 class UserModelTest(TestCase):
     @classmethod
     def setUpTestData(cls):
-        user = User.objects.create_user("test")
+        User.objects.create_user("test")
 
     def test_user_create(self):
         user = User.objects.get(id=1)
