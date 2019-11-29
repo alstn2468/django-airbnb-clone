37fdfd1b6c5cadab05d1fb25dd8fe3c7e5ddcb9f
alstn2468 <alstn2468_@naver.com>
2019-11-29 21:07:25 +0900

    #51 Add some fields in User model and write test code user model's fields

--- users/models.py
+++ users/models.py
@@ -3,4 +3,28 @@ from django.db import models
 
 
 class User(AbstractUser):
-    bio = models.TextField(default="")
+    """Custom User Model
+    Inherit:
+        AbstractUser
+
+    Fields:
+        avatar : User profile image
+        gender : User gender
+        bio    : User profile description
+    """
+
+    GENDER_MALE = "male"
+    GENDER_FEMALE = "female"
+    GENDER_OTHER = "other"
+
+    GENDER_CHOICES = (
+        (GENDER_MALE, "Male"),
+        (GENDER_FEMALE, "Female"),
+        (GENDER_OTHER, "Other"),
+    )
+
+    avatar = models.ImageField(null=True, blank=True)
+    gender = models.CharField(
+        choices=GENDER_CHOICES, max_length=10, null=True, blank=True
+    )
+    bio = models.TextField(default="", blank=True)
--- users/tests/test_models.py
+++ users/tests/test_models.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
-from django.db import IntegrityError
+from django.db import IntegrityError, models
 from users.models import User
+import tempfile
 
 
 class UserModelTest(TestCase):
@@ -8,8 +9,13 @@ class UserModelTest(TestCase):
     def setUpTestData(cls):
         User.objects.create_user("test_user_1")
 
+    def setUp(self):
+        User.objects.create_user(
+            username="test_user_2", bio="test bio", gender=User.GENDER_MALE,
+        )
+
     def test_user_create_success(self):
-        user = User.objects.create_user("test_user_2")
+        user = User.objects.get(username="test_user_2")
         self.assertEqual("test_user_2", user.username)
         self.assertEqual("User", user.__class__.__name__)
 
@@ -17,11 +23,32 @@ class UserModelTest(TestCase):
         with self.assertRaises(IntegrityError):
             User.objects.create_user("test_user_1")
 
-    def test_user_bio_default_text(self):
-        user = User.objects.get(id=1)
+    def test_user_bio_default(self):
+        user = User.objects.get(username="test_user_1")
         self.assertEqual("", user.bio)
 
-    def test_user_bio_set_text(self):
-        user = User(username="test_user_3", bio="test bio")
+    def test_user_bio_set(self):
+        user = User.objects.get(username="test_user_2")
         self.assertEqual("test bio", user.bio)
+
+    def test_user_avatar_blank(self):
+        user = User.objects.get(username="test_user_1")
+        self.assertFalse(bool(user.avatar))
+
+    def test_user_avatar_set(self):
+        user = User.objects.get(username="test_user_2")
+
+        test_image_file = tempfile.NamedTemporaryFile(suffix=".jpg").name
+        user.avatar = test_image_file
+        user.save()
+
+        self.assertEqual(user.avatar, test_image_file)
+
+    def test_user_gender_blank(self):
+        user = User.objects.get(username="test_user_1")
+        self.assertIsNone(user.gender)
+
+    def test_user_gender_set(self):
+        user = User.objects.get(username="test_user_2")
+        self.assertEqual("male", user.gender)
 
