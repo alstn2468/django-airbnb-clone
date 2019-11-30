637728cb843e51d7a5388feb6dbd8285e1b59f00
alstn2468 <alstn2468_@naver.com>
2019-11-30 18:02:27 +0900

    #62 Add Room model in admin panel

--- rooms/admin.py
+++ rooms/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Room
 
-# Register your models here.
+
+@admin.register(Room)
+class RoomAdmin(admin.ModelAdmin):
+    """Register Room model at admin panel
+    
+    inherit:
+        ModelAdmin
+    """
+
+    pass
