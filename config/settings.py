7168edcd8bfebbba9b27724ec4eda27a088fd124
alstn2468 <alstn2468_@naver.com>
2019-11-30 19:27:20 +0900

    #67 Add THIRD_PARTY_APP django_countires

--- config/settings.py
+++ config/settings.py
@@ -41,13 +41,15 @@ DJANGO_APPS = [
     "django.contrib.staticfiles",
 ]
 
+THIRD_PARTY_APPS = ["django_countries"]
+
 PROJECT_APPS = [
     "users.apps.UsersConfig",
     "rooms.apps.RoomsConfig",
     "core.apps.CoreConfig",
 ]
 
-INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + PROJECT_APPS
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
