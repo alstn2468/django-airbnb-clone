7c7bc0be325525859084ce41b052f8ec77e44354
alstn2468 <alstn2468_@naver.com>
2019-11-30 19:30:17 +0900

    #68 Write TimeStamp model and create test_models.py

--- core/models.py
+++ core/models.py
@@ -2,14 +2,18 @@ from django.db import models
 
 
 class TimeStamp(models.Model):
-    """TimeStamp Model
+    """Abstract TimeStamp Model
 
     Inherit:
         Model
+
+    Fields:
+        created_at : DateTImeField (UnEditable)
+        updated_at : DateTimeField (Editable)
     """
 
-    created_at = models.DateTimeField()
-    updated_at = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         abstract = True
