4cafd332671036c4901314668718d212aefe6d34
alstn2468 <alstn2468_@naver.com>
2019-11-30 17:28:25 +0900

    #54 Set UserAdmin's fieldsets BaseUserAdmin's fieldsets + Custom User model's fieldsets

--- users/admin.py
+++ users/admin.py
@@ -1,10 +1,33 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from users.models import User
 
 
 @admin.register(User)
-class UserAdmin(admin.ModelAdmin):
+class UserAdmin(BaseUserAdmin):
     """Register User model at admin panel
+    
+    inherit:
+        UserAdmin as BaseUserAdmin
+
+    user_fieldsets : Custom User model's fieldsets
+    Set fieldsets BaseUserAdmin's fieldsets + user_fieldsets
     """
 
-    pass
+    user_fieldsets = (
+        (
+            "Custom Profile",
+            {
+                "fields": (
+                    "avatar",
+                    "gender",
+                    "bio",
+                    "birth_date",
+                    "language",
+                    "currency",
+                    "is_superhost",
+                )
+            },
+        ),
+    )
+    fieldsets = BaseUserAdmin.fieldsets + user_fieldsets
