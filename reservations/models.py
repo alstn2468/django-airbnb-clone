35a615d9978721f60e6dac176496a608f5818d86
alstn2468 <alstn2468_@naver.com>
2019-12-03 17:57:08 +0900

    #94 Write Reservation model class

--- reservations/models.py
+++ reservations/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from core.models import AbstractTimeStamp
 
-# Create your models here.
+
+class Reservation(AbstractTimeStamp):
+    """Reservation Model
+
+    Inherit:
+        AbstractTimeStamp
+
+    Fields:
+        status    : CharField
+        check_in  : DateField
+        check_out : DateField
+        guest     : User Model (1:N)
+        room      : Room Model (1:N)
+    """
+
+    STATUS_PENDING = "pending"
+    STATUS_CONFIRMED = "confirmed"
+    STATUS_CANCELED = "canceled"
+
+    STATUS_CHOICES = (
+        (STATUS_PENDING, "Pending"),
+        (STATUS_CONFIRMED, "Confirmed"),
+        (STATUS_CANCELED, "Canceled"),
+    )
+
+    status = models.CharField(
+        max_length=12, choices=STATUS_CHOICES, default=STATUS_PENDING
+    )
+    check_in = models.DateField()
+    check_out = models.DateField()
+    guest = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    room = models.ForeignKey("rooms.Room", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.room} - {self.check_in}"
