7b2beea5451c96ed8589df64c7d1886da6e04603
alstn2468 <alstn2468_@naver.com>
2019-12-03 15:21:21 +0900

    #91 Fix Review model str method and fix test code

--- reviews/models.py
+++ reviews/models.py
@@ -1,14 +1,34 @@
 from django.db import models
+from core.models import AbstractTimeStamp
 
 
-class Review(models.Model):
+class Review(AbstractTimeStamp):
     """Review model
 
     Inherit:
-        Model
+        AbstractTimeStamp
 
     Fields:
-        
+        review        : TextField
+        accuracy      : IntegerField
+        communication : IntegerField
+        cleanliness   : IntegerField
+        location      : IntegerField
+        check_in      : IntegerField
+        value         : IntegerField
+        user          : User Model (1:N)
+        room          : Room Model (1:N)
     """
 
-    pass
+    review = models.TextField()
+    accuracy = models.IntegerField()
+    communication = models.IntegerField()
+    cleanliness = models.IntegerField()
+    location = models.IntegerField()
+    check_in = models.IntegerField()
+    value = models.IntegerField()
+    user = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    room = models.ForeignKey("rooms.Room", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.review} - {self.room}"
--- reviews/tests/test_models.py
+++ reviews/tests/test_models.py
@@ -1,4 +1,11 @@
 from django.test import TestCase
+from django.db import IntegrityError
+from reviews.models import Review
+from users.models import User
+from rooms.models import Room
+from datetime import datetime
+from unittest import mock
+import pytz
 
 
 class ReviewModelTest(TestCase):
@@ -7,29 +14,106 @@ class ReviewModelTest(TestCase):
         """Run only once when running ReviewModelTest
 
         Fields :
+            id            : 1
+            review        : Test Review 1
+            accuracy      : 1
+            communication : 1
+            cleanliness   : 2
+            location      : 2
+            check_in      : 3
+            value         : 3
+            user          : test_user
+            room          : test_room
+            created_at    : 2019.11.30.00.00.00
+            updated_at    : 2019.12.01.00.00.00
         """
-        pass
+        user = User.objects.create_user("test_user")
+        room = Room.objects.create(
+            name="Test Room",
+            description="Test Description 1",
+            country="KR",
+            city="Seoul",
+            price=100,
+            address="Test Address",
+            guests=4,
+            beds=2,
+            bedrooms=1,
+            baths=1,
+            check_in=datetime(2019, 1, 1, 9, 30),
+            check_out=datetime(2019, 1, 2, 10, 30),
+            instant_book=True,
+            host=user,
+        )
+        mocked = datetime(2019, 11, 30, 0, 0, 0, tzinfo=pytz.utc)
+
+        with mock.patch("django.utils.timezone.now", mock.Mock(return_value=mocked)):
+            Review.objects.create(
+                review="Test Review 1",
+                accuracy=1,
+                communication=1,
+                cleanliness=2,
+                location=2,
+                check_in=3,
+                value=3,
+                user=user,
+                room=room,
+            )
 
     def test_review_create_success(self):
         """Review model creation success test
         Check unique field and instance's class name
         """
-        pass
+        review = Review.objects.get(id=1)
+        self.assertEqual("Review", review.__class__.__name__)
+        self.assertEqual("Test Review 1", review.review)
+        self.assertEqual("test_user", review.user.username)
+        self.assertEqual("Test Room", review.room.name)
 
     def test_review_create_fail(self):
         """Review model creation failure test
-        Duplicate pk index with IntegrityError exception
+        Duplicate user & room with IntegrityError exception
         """
-        pass
+        with self.assertRaises(IntegrityError):
+            user = User.objects.get(id=1)
+            room = Room.objects.get(id=1)
+            Review.objects.create(review="Test Review2", user=user, room=room)
 
     def test_review_get_fields(self):
         """Review model get fields data test
         Check review's all fields except when testing create success test
         """
-        pass
+        review = Review.objects.get(id=1)
+        self.assertEqual(review.accuracy, 1)
+        self.assertEqual(review.communication, 1)
+        self.assertEqual(review.cleanliness, 2)
+        self.assertEqual(review.location, 2)
+        self.assertEqual(review.check_in, 3)
+        self.assertEqual(review.value, 3)
 
     def test_review_str_method(self):
         """Review model str method test
         CHeck str mehtod equal review instance name field
         """
-        pass
+        review = Review.objects.get(id=1)
+        self.assertEqual("Test Review 1 - Test Room", str(review))
+
+    def test_review_time_stamp_created_at(self):
+        """TimeStamp model created_at test
+        Check Test Review 1's created_at field is datetime (2019.11.30)
+        """
+        review = Review.objects.get(id=1)
+        mocked = datetime(2019, 11, 30, 0, 0, 0, tzinfo=pytz.utc)
+        self.assertEqual(review.created_at, mocked)
+
+    def test_review_time_stamp_updated_at(self):
+        """TimeStamp model updated_at test
+        Get Test Review 1 objects and update Test Review 1 object
+        Check Test Review 1's updated_at field is datetime (2019.12.01)
+        """
+        review = Review.objects.get(id=1)
+        mocked = datetime(2019, 12, 1, 0, 0, 0, tzinfo=pytz.utc)
+
+        with mock.patch("django.utils.timezone.now", mock.Mock(return_value=mocked)):
+            review.location = 1
+            review.save()
+            self.assertEqual(review.updated_at, mocked)
