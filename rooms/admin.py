4d638bb41f6002f8aa43ae05b8765878259dd186
alstn2468 <alstn2468_@naver.com>
2019-12-05 20:59:21 +0900

    #104 Set RoomAdmin class' list_display, list_filter, search_fields option

--- rooms/admin.py
+++ rooms/admin.py
@@ -4,9 +4,34 @@ from .models import Room, RoomType, Amenity, Facility, HouseRule, Photo
 
 @admin.register(Room)
 class RoomAdmin(admin.ModelAdmin):
-    """Register Room model at admin panel"""
+    """Register Room model at admin panel
 
-    pass
+    Filter by:
+        instant_book : BooleanField
+        city         : CharField
+        country      : CharField
+
+    Search by:
+        city          : exact
+        host.username : startwith
+    """
+
+    list_display = (
+        "name",
+        "country",
+        "city",
+        "price",
+        "address",
+        "guests",
+        "beds",
+        "bedrooms",
+        "baths",
+        "check_in",
+        "check_out",
+        "instant_book",
+    )
+    list_filter = ("instant_book", "city", "country")
+    search_fields = ("=city", "^host__username")
 
 
 @admin.register(RoomType, Amenity, Facility, HouseRule)
