312ddc62d4c697f3e10569d45f7a34903cd68eb1
alstn2468 <alstn2468_@naver.com>
2019-12-11 20:49:34 +0900

    #118 Add list_display & list_filter options at ReservationAdmin

--- reservations/admin.py
+++ reservations/admin.py
@@ -6,4 +6,14 @@ from reservations.models import Reservation
 class ReservationAdmin(admin.ModelAdmin):
     """Register Reservation model at admin panel"""
 
-    pass
+    list_display = (
+        "room",
+        "status",
+        "check_in",
+        "check_out",
+        "guest",
+        "in_progress",
+        "is_finished",
+    )
+
+    list_filter = ("status",)
