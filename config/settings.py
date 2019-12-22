8a51af0992cc75fda9bcc437cbea758fcc18d079
alstn2468 <alstn2468_@naver.com>
2019-12-22 22:37:47 +0900

    #141 Install django_seed third party app

--- config/settings.py
+++ config/settings.py
@@ -41,7 +41,7 @@ DJANGO_APPS = [
     "django.contrib.staticfiles",
 ]
 
-THIRD_PARTY_APPS = ["django_countries"]
+THIRD_PARTY_APPS = ["django_countries", "django_seed"]
 
 PROJECT_APPS = [
     "users.apps.UsersConfig",
