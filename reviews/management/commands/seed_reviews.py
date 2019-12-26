037f061fc2038ac0aeb9bef458ea844b541cd702
alstn2468 <alstn2468_@naver.com>
2019-12-26 20:06:41 +0900

    #152 Create sedd_reviews command to create reviews automatically for each rooms

--- reviews/management/commands/seed_reviews.py
+++ reviews/management/commands/seed_reviews.py
@@ -1,8 +1,9 @@
 from core.management.commands.custom_command import CustomCommand
 from django_seed import Seed
-from random import randint
+from random import randint, choice
 from reviews.models import Review
 from rooms.models import Room
+from users.models import User
 
 
 class Command(CustomCommand):
@@ -17,14 +18,38 @@ class Command(CustomCommand):
 
             self.stdout.write(self.style.SUCCESS("■ START CREATE REVIEWS"))
 
-            for i in range(100):
-                self.stdout.write("%{}".format(i), ending="\r")
-                self.stdout.flush()
-                time.sleep(0.01)
+            users = User.objects.all()
+            rooms = Room.objects.all()
+
+            for idx, room in enumerate(rooms):
+                seeder = Seed.seeder()
+                seeder.add_entity(
+                    Review,
+                    number,
+                    {
+                        "accuracy": lambda x: randint(0, 6),
+                        "communication": lambda x: randint(0, 6),
+                        "cleanliness": lambda x: randint(0, 6),
+                        "location": lambda x: randint(0, 6),
+                        "check_in": lambda x: randint(0, 6),
+                        "value": lambda x: randint(0, 6),
+                        "room": room,
+                        "user": lambda x: choice(users),
+                    },
+                )
+
+                self.progress_bar(
+                    idx + 1,
+                    len(rooms),
+                    prefix="■ PROGRESS",
+                    suffix="Complete",
+                    length=40,
+                )
+
+                seeder.execute()
 
             self.stdout.write(self.style.SUCCESS("■ SUCCESS CREATE ALL REVIEWS!"))
 
         except Exception as e:
             self.stdout.write(self.style.ERROR(f"■ {e}"))
             self.stdout.write(self.style.ERROR("■ FAIL CREATE REVIEWS"))
-
