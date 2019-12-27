513539141c547c66d34ef63184932f3f4b3d81bc
alstn2468 <alstn2468_@naver.com>
2019-12-27 17:43:35 +0900

    #155 Create seed_resservations command to create reservation automatically

---
+++ reservations/management/commands/seed_reservations.py
@@ -0,0 +1,51 @@
+from core.management.commands.custom_command import CustomCommand
+from django_seed import Seed
+from datetime import datetime, timedelta
+from random import randint, choice
+from rooms.models import Room
+from users.models import User
+from reservations.models import Reservation
+
+
+class Command(CustomCommand):
+    help = "Automatically create reservations"
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--number", default=1, help="Number of reservations to create"
+        )
+
+    def handle(self, *args, **options):
+        try:
+            number = int(options.get("number"))
+
+            self.stdout.write(self.style.SUCCESS("■ START CREATE RESERVATIONS"))
+
+            users = User.objects.all()
+            rooms = Room.objects.all()
+
+            seeder = Seed.seeder()
+            seeder.add_entity(
+                Reservation,
+                number,
+                {
+                    "status": choice(
+                        [
+                            Reservation.STATUS_CANCELED,
+                            Reservation.STATUS_CONFIRMED,
+                            Reservation.STATUS_PENDING,
+                        ]
+                    ),
+                    "guest": choice(users),
+                    "room": choice(rooms),
+                    "check_in": datetime.now(),
+                    "check_out": datetime.now() + timedelta(days=randint(3, 25)),
+                },
+            )
+            seeder.execute()
+
+            self.stdout.write(self.style.SUCCESS("■ SUCCESS CREATE ALL RESERVATIONS!"))
+
+        except Exception as e:
+            self.stdout.write(self.style.ERROR(f"■ {e}"))
+            self.stdout.write(self.style.ERROR("■ FAIL CREATE RESERVATIONS"))
