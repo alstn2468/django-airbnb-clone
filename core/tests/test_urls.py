1593240e75b378351ab23c1206d08804db06dc05
alstn2468 <alstn2468_@naver.com>
2019-12-28 20:17:24 +0900

    #158 Fix tests.py -> core/test_urls.py

--- core/tests/test_urls.py
+++ core/tests/test_urls.py
@@ -3,7 +3,7 @@ from django.urls import resolve
 from rooms.views import all_rooms
 
 
-class CoreApplicationTest(TestCase):
+class CoreUrlTest(TestCase):
     def test_root_url_resolves_to_all_rooms(self):
         """Core application '/' pattern urls test
         Check '/' pattern resolved func is all_rooms
