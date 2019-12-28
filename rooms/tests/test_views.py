42fe10952936ed5d73fd735de1fdf86764288f60
alstn2468 <alstn2468_@naver.com>
2019-12-28 20:18:00 +0900

    #159 Fix test class name RoomApplicationViewTest -> RoomViewTest

--- rooms/tests/test_views.py
+++ rooms/tests/test_views.py
@@ -3,7 +3,7 @@ from django.http import HttpRequest
 from rooms.views import all_rooms
 
 
-class RoomApplicationViewTest(TestCase):
+class RoomViewTest(TestCase):
     def test_rooms_app_all_rooms_view(self):
         """Rooms application all_rooms view test
         Check all_rooms return value is None
