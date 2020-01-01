1f34d6155a16cc325e18530ccb8daf5d0554589a
alstn2468 <alstn2468_@naver.com>
2020-01-01 19:09:39 +0900

    #165 Apply pagination all_rooms view func and Add test case

--- rooms/tests/test_views.py
+++ rooms/tests/test_views.py
@@ -1,17 +1,55 @@
-from django.test import TestCase
-from django.http import HttpRequest
+from django.test import TestCase, Client
 from rooms.views import all_rooms
 from rooms.models import Room
+from users.models import User
+from datetime import datetime
 
 
 class RoomViewTest(TestCase):
-    def test_rooms_app_all_rooms_view(self):
-        """Rooms application all_rooms view test
+    @classmethod
+    def setUpTestData(cls):
+        """Run only once when running RoomViewTest
+        Create 13 rooms for RoomViewTest
+        """
+        user = User.objects.create_user("test_user")
+
+        for i in range(1, 14):
+            Room.objects.create(
+                name=f"Test Room {i}",
+                description="Test Description",
+                country="KR",
+                city="Seoul",
+                price=100,
+                address="Test Address",
+                guests=4,
+                beds=2,
+                bedrooms=1,
+                baths=1,
+                check_in=datetime(2019, 1, 1, 9, 30),
+                check_out=datetime(2019, 1, 2, 10, 30),
+                instant_book=True,
+                host=user,
+            )
+
+    def test_view_rooms_app_all_rooms_default_page(self):
+        """Rooms application all_rooms view test without pagination param
         Check all_rooms HttpResponse content data contain right data
         """
-        request = HttpRequest()
-        response = all_rooms(request)
-        rooms = Room.objects.all()
+        response = self.client.get("/")
+        rooms = Room.objects.all()[:10]
+
+        html = response.content.decode("utf8")
+        self.assertIn("<title>HOME | Airbnb</title>", html)
+
+        for room in rooms:
+            self.assertIn(f"<h1>{room} / ${room.price}</h1>", html)
+
+    def test_view_rooms_app_all_rooms_next_page(self):
+        """Rooms application all_rooms view test with pagination param
+        Check all_rooms HttpResponse content data contain right data
+        """
+        response = self.client.get("/", {"page": 2})
+        rooms = Room.objects.all()[10:13]
 
         html = response.content.decode("utf8")
         self.assertIn("<title>HOME | Airbnb</title>", html)
--- rooms/views.py
+++ rooms/views.py
@@ -3,6 +3,12 @@ from rooms.models import Room
 
 
 def all_rooms(request):
-    rooms = Room.objects.all()
+    page = int(request.GET.get("page", 1))
+    PAGE_SIZE = 10
+
+    LIMIT = PAGE_SIZE * page
+    OFFSET = LIMIT - PAGE_SIZE
+
+    rooms = Room.objects.all()[OFFSET:LIMIT]
 
     return render(request, "rooms/home.html", {"rooms": rooms})
