944a31083bbbf1047cfc4871b1099e1eebb26218
alstn2468 <alstn2468_@naver.com>
2020-01-04 22:37:57 +0900

    #169 Using django built in paginator module for main page

--- rooms/views.py
+++ rooms/views.py
@@ -1,21 +1,14 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from rooms.models import Room
 from math import ceil
 
 
 def all_rooms(request):
     page = request.GET.get("page", 1)
-    page = int(page or 1)
+    room_qs = Room.objects.all()
 
-    PAGE_SIZE = 10
-    LIMIT = PAGE_SIZE * page
-    OFFSET = LIMIT - PAGE_SIZE
+    paginator = Paginator(room_qs, 10)
+    rooms = paginator.get_page(page)
 
-    rooms = Room.objects.all()[OFFSET:LIMIT]
-    page_count = ceil(Room.objects.count() / PAGE_SIZE)
-
-    return render(
-        request,
-        "rooms/home.html",
-        {"rooms": rooms, "page": page, "page_count": page_count,},
-    )
+    return render(request, "rooms/home.html", {"rooms": rooms})
