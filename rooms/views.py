6c929c618128177919710e067fb770542e529800
alstn2468 <alstn2468_@naver.com>
2020-01-05 17:26:37 +0900

    #171 Using previous_page_number and next_page_number at home.html

--- rooms/views.py
+++ rooms/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.core.paginator import Paginator
 from rooms.models import Room
-from math import ceil
 
 
 def all_rooms(request):
@@ -11,4 +10,4 @@ def all_rooms(request):
     paginator = Paginator(room_qs, 10)
     rooms = paginator.get_page(page)
 
-    return render(request, "rooms/home.html", {"rooms": rooms})
+    return render(request, "rooms/home.html", {"pages": rooms})
