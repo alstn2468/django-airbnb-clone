13fa2e8c45bcd5495e1f17b4a5c63c97b8a3bdc5
Kimminsu <alstn2468_@naver.com>
2020-02-18 09:30:03 +0900

    #190 Fix rooms/test_urls.py RoomUrlTest docstring

--- rooms/tests/test_urls.py
+++ rooms/tests/test_urls.py
@@ -5,14 +5,14 @@ from rooms.views import RoomDetail, search
 
 class RoomsUrlTest(TestCase):
     def test_url_resolves_to_room_deatil(self):
-        """Core application '/rooms/1' pattern urls test
+        """Room application '/rooms/1' pattern urls test
         Check '/rooms/1' pattern resolved class is RoomDetail
         """
         found = resolve("/rooms/1")
         self.assertEqual(found.func.view_class, RoomDetail)
 
     def test_url_resolves_to_seacrh(self):
-        """Core application '/rooms/search/' pattern urls test
+        """Room application '/rooms/search/' pattern urls test
         Check '/rooms/search/' pattern resolved function is search
         """
         found = resolve("/rooms/search/")
