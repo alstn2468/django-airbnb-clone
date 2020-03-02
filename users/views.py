7761852157340ea608ed030162e9ec397a9b01fb
Kimminsu <alstn2468_@naver.com>
2020-03-02 12:41:11 +0900

    #220 Fix LoginView parent class is FormView

--- users/views.py
+++ users/views.py
@@ -1,41 +1,36 @@
-from django.views import View
+from django.views.generic import FormView
+from django.urls import reverse_lazy
 from django.shortcuts import render, redirect, reverse
 from django.contrib.auth import authenticate, login, logout
 from users.forms import LoginForm
 
 
-class LoginView(View):
+class LoginView(FormView):
     """users application LoginView class
 
-    Inherit             : View
-    Templates name      : users/login.html
+    Inherit       : FormView
+    template_name : "users/login.html"
+    form_class    : LoginForm
+    success_url   : reverse_lazy("core:home")
 
     Method:
-        get  : rendering empty login form
-        post : login success redirect home
-               login fail render login page with form data
+        form_valid : user auth process
     """
 
-    def get(self, request):
-        form = LoginForm()
+    template_name = "users/login.html"
+    form_class = LoginForm
+    success_url = reverse_lazy("core:home")
 
-        return render(request, "users/login.html", {"form": form})
+    def form_valid(self, form):
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
 
-    def post(self, request):
-        form = LoginForm(request.POST)
+        user = authenticate(self.request, username=email, password=password)
 
-        if form.is_valid():
-            email = form.cleaned_data.get("email")
-            password = form.cleaned_data.get("password")
+        if user is not None:
+            login(self.request, user)
 
-            user = authenticate(request, username=email, password=password)
-
-            if user is not None:
-                login(request, user)
-
-                return redirect(reverse("core:home"))
-
-        return render(request, "users/login.html", {"form": form})
+        return super().form_valid(form)
 
 
 def log_out(request):
