4d314323a5f8c8e6f2aa14d032fd570fa7004d18
Kimminsu <alstn2468_@naver.com>
2020-06-15 11:37:48 +0900

    #262 Extend tailwind spacing, border-radius and Add main container img

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 module.exports = {
-  purge: [],
-  theme: {
-    extend: {},
-  },
-  variants: {},
-  plugins: [],
-}
+    purge: [],
+    theme: {
+        extend: {
+            spacing: {
+                "25vh": "25vh",
+                "50vh": "50vh",
+                "75vh": "75vh",
+            },
+            borderRadius: {
+                xl: "1.5rem",
+            },
+        },
+    },
+    variants: {},
+    plugins: [],
+};
