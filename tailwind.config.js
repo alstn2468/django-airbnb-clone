042119f76c271e422587c6f11468ae3950394bdc
alst2468 <alstn2468_@naver.com>
2020-10-28 21:24:04 +0900

    #287 Add user detail room card styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,31 +1,35 @@
 module.exports = {
-    purge: [],
-    theme: {
-        extend: {
-            spacing: {
-                "25vh": "25vh",
-                "50vh": "50vh",
-                "75vh": "75vh",
-            },
-            borderRadius: {
-                xl: "1.5rem",
-            },
-            backgroundSize: {
-                sm: "16px 16px",
-                md: "20px 20px",
-                lg: "22px 22px",
-                xl: "24px 24px",
-            },
-            backgroundPosition: {
-                "left-sm": "7px 7px",
-                "left-lg": "10px 7px",
-            },
-            colors: {
-                kakao: "#ffeb00",
-                airbnb: "rgb(255, 90, 95)",
-            },
-        },
+  purge: [],
+  theme: {
+    extend: {
+      spacing: {
+        '25vh': '25vh',
+        '50vh': '50vh',
+        '75vh': '75vh',
+      },
+      borderRadius: {
+        xl: '1.5rem',
+      },
+      minHeight: {
+        '50vh': '50vh',
+        '75vh': '75vh',
+      },
+      backgroundSize: {
+        sm: '16px 16px',
+        md: '20px 20px',
+        lg: '22px 22px',
+        xl: '24px 24px',
+      },
+      backgroundPosition: {
+        'left-sm': '7px 7px',
+        'left-lg': '10px 7px',
+      },
+      colors: {
+        kakao: '#ffeb00',
+        airbnb: 'rgb(255, 90, 95)',
+      },
     },
-    variants: {},
-    plugins: [],
+  },
+  variants: {},
+  plugins: [],
 };
