b027bb7b74a4a96bd17ce0ddc614511b6258f8f9
alstn2468 <alstn2468_@naver.com>
2021-02-07 22:13:58 +0900

    #301 Add get_beds method at user model, styling romm detail tempalte

--- rooms/models.py
+++ rooms/models.py
@@ -184,3 +184,8 @@ class Room(AbstractTimeStamp):
     def get_next_four_photos(self):
         photos = self.photos.all()[1:5]
         return photos
+
+    def get_beds(self):
+        if self.beds == 1:
+            return "1 bed"
+        return f"{self.beds} beds"
